83fc85131846d51e90f45247541c145d261a2201
JoacoOli <joaquinoliverosgi@gmail.com>
2023-12-07 11:37:48 -0300

    proyect

---
+++ Caso9_Centro_de_Bowling/Caso9_Centro_de_Bowling/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Caso9_Centro_de_Bowling project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Caso9_Centro_de_Bowling.settings')
+
+application = get_wsgi_application()
